c7cd3ca920b034788d43cd59e601bc4ba453c68f
Alan Yang <alany3@gmail.com>
2019-01-02 21:24:30 -0500

    Fix diff-highlight

--- brew/install.sh
+++ brew/install.sh
@@ -8,3 +8,5 @@ brew cask install spectacle
 # brews
 brew install diff-so-fancy
 brew install git
+
+git config --global core.pager "`brew --prefix git`/share/git-core/contrib/diff-highlight/diff-highlight | less"
