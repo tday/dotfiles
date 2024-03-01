55081ebac20e47239a98b2c4db70aa91e877280d
Alan Yang <alan@rayfit.com>
2024-03-01 10:10:43 -0500

    fix: update brew and zsh

--- brew/install.sh
+++ brew/install.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-command -v brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# casks
-brew cask install spectacle
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # brews
 brew install diff-so-fancy
--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -2,3 +2,18 @@
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp tday.zsh-theme ~/.oh-my-zsh/themes/
+
+echo "Installing powerline fonts"
+git clone https://github.com/powerline/fonts.git --depth=1
+pushd fonts
+./install.sh
+popd
+rm -rf fonts
+
+PURPLE='\033[0;35m'
+NC='\033[0m'
+echo -e "${PURPLE}"
+echo "/**********************************************************************\\"
+echo "Installed powerline fonts! Don't forget to update the font in terminal"
+echo "\\**********************************************************************/"
+echo -e "${NC}"
